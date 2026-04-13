7e1d02fa83b1a7fb276508930b793386a4fba6b1
pancake0003 <songyuemmm@gmail.com>
2026-04-13 18:24:59 -0400

    fixed linting issues

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,15 @@
+"""web app for the project"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def dashboard():
+def index():
+    """render home page"""
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
